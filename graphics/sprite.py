f0fd72e8d96bb0b1d6f319f7653aa78aab6a68f3
Paul Robson <paul@robsons.org.uk>
2020-11-21 17:12:05 +0000

    Update

--- graphics/sprite.py
+++ graphics/sprite.py
@@ -9,6 +9,7 @@
 # *******************************************************************************************
 # *******************************************************************************************
 
+import os
 from PIL import Image												# PILLOW PREQREQUISITE
 
 # *******************************************************************************************
@@ -18,8 +19,9 @@ from PIL import Image												# PILLOW PREQREQUISITE
 # *******************************************************************************************
 
 class SpriteConverter(object):
-	def __init__(self):
+	def __init__(self,handler = None):
 		self.palette = self.getPalette()							# subclass for different palettes.
+		self.handler = handler 										# what we do with it.
 		assert len(self.palette) == 15 								# 15 colours + transparent.
 	#
 	#		Convert one sprite.
@@ -36,7 +38,17 @@ class SpriteConverter(object):
 					col = self.getBestColour(rgba) 					# what colour fits best ?
 																	# add into MSN/LSN.
 					spriteData[y*(width >> 1)+(x>>1)] += (col if x%2 != 0 else (col << 4))
+		if self.handler is not None:								# do we have something to do ?
+			name = os.path.basename(imageFile)
+			name = name[:name.rfind(".")]
+			self.handler.handle(name,spriteData,width,height)
 		return spriteData
+	#
+	#		Complete
+	#
+	def done(self):
+		if self.handler is not None:
+			self.handler.close()
 	#
 	#		Match RGBA colour against palette
 	#
@@ -54,7 +66,25 @@ class SpriteConverter(object):
 	#		is not square it will be aligned accordingly.
 	#
 	def preProcess(self,image,width,height,alignment):
-		return image
+		if width == image.size[0] and height == image.size[1]:		# correct size ?
+			return image
+		scale = min(width/image.size[0],height/image.size[1])		# scale it first ?
+		if scale != 1.0:											# scale image keeping a/r
+			image = self.scaleImage(image,width*scale,height*scale)	# rescale it
+		newImage = Image.new("RGBA",(width,height),0x0) 			# Fill with clear.
+		x = int(newImage.size[0]/2-image.size[0]/2) 				# centre horizontally.
+		y = int(newImage.size[1]/2-image.size[1]/2) 				# centre vertically
+		if alignment == SpriteConverter.CENTREBOTTOM:				# put on the bottom.
+			y = height-image.size[1]
+		newImage.paste(image,(x,y))
+		return newImage
+	#
+	#		Scale Image 
+	#
+	def scaleImage(self,image,width,height):
+		width = int(width+0.5)
+		height = int(height+0.5)
+		return image.resize((width,height))
 
 SpriteConverter.CENTRE = 'C'										# centred
 SpriteConverter.CENTREBOTTOM = 'B'									# centred horizontally bottom vertically
@@ -81,10 +111,38 @@ class ImprovedSpriteConverter(SpriteConverter):
 		palette = [[0, 0, 0], [255, 0, 0], [0, 255, 0], [255, 255, 0], [0, 0, 255], [255, 0, 255], [0, 255, 255], [255, 255, 255], [0, 0, 0], [87, 87, 87], [160, 160, 160], [255, 128, 0], [150, 70, 20], [128, 255, 0], [0, 128, 255], [255, 205, 243]]
 		return palette[1:]
 
+# *******************************************************************************************
+#
+#								Sprite Output handler Base
+#
+# *******************************************************************************************
 
-sc = ImprovedSpriteConverter()
-spriteData = sc.convert("mario.png",64,64,SpriteConverter.CENTRE)
-h = open("sprites.dat","w")
-for c in spriteData:
-	h.write("{0:02x}".format(c))
-h.close()
+class SpriteOutputHandler(object):
+	def __init__(self):
+		pass
+	def close(self):
+		pass
+	def handle(self,name,imageData,width,height):
+		pass
+
+# *******************************************************************************************
+#
+#								  Amoral inline handler
+#
+# *******************************************************************************************
+
+class AmoralInlineHandler(object):
+	def __init__(self,fileName = "sprite.data.amo"):
+		self.h = open(fileName,"w")
+	def close(self):
+		self.h.close()
+	def handle(self,name,imageData,width,height):
+		hexData = "".join(["{0:02x}".format(c) for c in imageData])
+		self.h.write('proc spr.{0}.{1}x{2}() {{\n[["{3}"]]\n}} \n\n'.format(name,width,height,hexData))
+
+sc = ImprovedSpriteConverter(AmoralInlineHandler())
+
+spriteData = sc.convert("mario.png",32,32,SpriteConverter.CENTREBOTTOM)
+spriteData = sc.convert("mario.png",64,64,SpriteConverter.CENTREBOTTOM)
+
+sc.done()
